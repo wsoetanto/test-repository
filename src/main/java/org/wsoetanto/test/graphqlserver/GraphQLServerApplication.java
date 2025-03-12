5d111415bdd50ed7fa461220ae318af0fd3480c1
chq-williams1 <william.soetanto@expeditors.com>
2025-03-12 16:07:02 -0700

    feat: random things

---
+++ src/main/java/org/wsoetanto/test/graphqlserver/GraphQLServerApplication.java
@@ -0,0 +1,12 @@
+package org.wsoetanto.test.graphqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQLServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQLServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wsoetanto/test/graphqlserver/GraphqlserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wsoetanto.test.graphqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
